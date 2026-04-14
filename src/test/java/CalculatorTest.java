381dff3d61be085c30ed5a69f0eddd9f104cce02
LyPhat <23130229@st.hcmuaf.edu.vn>
2026-04-14 23:31:23 +0700

    feat: initial project with tests

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+ public class CalculatorTest {
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
